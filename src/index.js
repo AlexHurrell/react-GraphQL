89edfe59deb8ca8409329269d65b34669b0d5a40
AlexHurrell <39062336+AlexHurrell@users.noreply.github.com>
2021-09-19 17:48:07 +0200

    graphql getting started

--- src/index.js
+++ src/index.js
@@ -1,17 +1,121 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { graphql } from "graphql";
+
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io",
+  cache: new InMemoryCache(),
+});
+
+// const server = mockServer(schema, {
+//   RootQuery: () => ({
+//     user: (o, { id }) => ({ id }),
+//   }),
+//   List: () => ({
+//     name: () => casual.word,
+//     tasks: () => new MockList(4, (o, { completed }) => ({ completed })),
+//   }),
+//   Task: () => ({ text: casual.words(10) }),
+//   User: () => ({ name: casual.name }),
+// });
+
+// const schema = `
+//   type User {
+//     id: ID!
+//     name: String
+//     lists: [List]
+//   }
+//   type List {
+//     id: ID!
+//     name: String
+//     owner: User
+//     incomplete_count: Int
+//     tasks(completed: Boolean): [Task]
+//   }
+//   type Task {
+//     id: ID!
+//     text: String
+//     completed: Boolean
+//     list: List
+//   }
+//   type RootQuery {
+//     user(id: ID): User
+//   }
+//   schema {
+//     query: RootQuery
+//   }
+// `;
+
+const schemaString = `...`;
+
+// Make a GraphQL schema with no resolvers
+const schema = makeExecutableSchema({ typeDefs: schemaString });
+
+// Create a new schema with mocks
+const schemaWithMocks = addMocksToSchema({ schema });
+
+const query = /* GraphQL */ `
+  query tasksForUser {
+    user(id: 6) {
+      id
+      name
+    }
+  }
+`;
+
+graphql(schemaWithMocks, query).then((result) =>
+  console.log("Got result", result)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      rates(currency: "USD") {
+        currency
+        rate
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <ol>
+      {data.rates.map(({ currency, rate }) => (
+        <li key={currency}>
+          <p>
+            {currency}: {rate}
+          </p>
+        </li>
+      ))}
+      ;
+    </ol>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <ExchangeRates />
+    </div>
+  );
+}
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
