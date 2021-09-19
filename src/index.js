6fdd35888e7d95d521d11a2e37a2b6ea1546a4b8
AlexHurrell <39062336+AlexHurrell@users.noreply.github.com>
2021-09-19 19:30:49 +0200

    added graphql/mocks

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import {
   useQuery,
   gql,
 } from "@apollo/client";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { graphql } from "graphql";
 
 const client = new ApolloClient({
   uri: "https://48p1r2roz4.sse.codesandbox.io",
@@ -54,4 +57,25 @@ render(
     <App />
   </ApolloProvider>,
   document.getElementById("root")
+);
+
+const schemaString = `...`;
+
+// Make a GraphQL schema with no resolvers
+const schema = makeExecutableSchema({ typeDefs: schemaString });
+
+// Create a new schema with mocks
+const schemaWithMocks = addMocksToSchema({ schema });
+
+const query = /* GraphQL */ `
+  query tasksForUser {
+    user(id: 6) {
+      id
+      name
+    }
+  }
+`;
+
+graphql(schemaWithMocks, query).then((result) =>
+  console.log("Got result", result)
 );
